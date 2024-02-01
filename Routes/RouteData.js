8e6313c8fc82344ecb1a283a676192d0757cb6b7
Ranjan-coder <ranjanchanda06@gmail.com>
2024-02-01 22:39:00 +0530

    login register route added

--- Routes/RouteData.js
+++ Routes/RouteData.js
@@ -1,6 +1,8 @@
 const dataRoute = require('express').Router()
 const {data} = require('../Config/db')
+const  routes  = require('./Rooute')
 
 dataRoute.get('/ecom',data)
+dataRoute.post('/ecom',routes)
 
 module.exports = {dataRoute}
